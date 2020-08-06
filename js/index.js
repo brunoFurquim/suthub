253e22366ff99fa5fec160c80532c9337af4d939
brunoFurquim <brunofurquimc@gmail.com>
2020-08-06 19:22:52 -0300

    pesquisa retornando idiomas do pais buscado e paises que possuem o mesmo idioma

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 let buscar = async (pais) => {
     let response = await axios.get(`https://restcountries.eu/rest/v2/name/${pais}`)
-    return response.data[0]
+    console.log("RESPONDEEEEEEEEEEEE", response)
+    let info = response.data.find(elemento => elemento.translations.br === pais)
+    return info !== undefined ? info : response.data[0]
 }
 
 let buscar_info_brasil = async () => await buscar('Brasil')
 
-
+let buscarIdioma = async (idioma) => {
+    let response = await axios.get(`https://restcountries.eu/rest/v2/lang/${idioma}`)
+    console.log(response)
+}
