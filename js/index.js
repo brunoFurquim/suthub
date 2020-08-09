e2680e6a5f83b0f4dc03b7806f5911b273171b49
brunoFurquim <brunofurquimc@gmail.com>
2020-08-09 16:45:31 -0300

    finalizacao blocos regionais e listagem de paises por b.r.

--- js/index.js
+++ js/index.js
@@ -5,9 +5,39 @@ let buscar = async (pais) => {
     return info !== undefined ? info : response.data[0]
 }
 
-let buscar_info_brasil = async () => await buscar('Brasil')
+let buscarInfoBrasil = async () => await buscar('Brasil')
 
 let buscarIdioma = async (idioma) => {
     let response = await axios.get(`https://restcountries.eu/rest/v2/lang/${idioma}`)
     return response.data;
+}
+
+let todosPaises = async() => {
+    let response = await axios.get('https://restcountries.eu/rest/v2/all')
+    return response.data
+}
+
+let listarBlocosRegionais = async () => {
+    let paises = await todosPaises()
+    paises = paises.map(pais => {
+        return pais.regionalBlocs[0] !== undefined ? pais.regionalBlocs[0] : undefined
+    })
+    
+    let blocosRegionais = []
+    paises.forEach(pais => {        
+        if (pais !== undefined) {
+            let paisObjeto = {
+                nome: pais.name,
+                acronimo: pais.acronym,
+            }
+            let index = blocosRegionais.findIndex(bloco => bloco.nome === paisObjeto.nome)
+            if (index === -1) blocosRegionais.push(paisObjeto)
+        }       
+    })
+    return blocosRegionais;
+}
+
+let buscarBlocoRegional = async (acronimo) => {
+    let response = await axios.get(`https://restcountries.eu/rest/v2/regionalbloc/${acronimo}`)
+    return response.data
 }
